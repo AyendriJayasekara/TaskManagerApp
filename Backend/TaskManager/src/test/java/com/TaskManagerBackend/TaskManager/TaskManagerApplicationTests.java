211b2eb48a1132cb3d56c37a9b4cffcf0fc6da65
AyendriJayasekara <jayasekaraayendri@gmail.com>
2025-04-05 09:27:09 +0530

    Added backend project to Backend folder

---
+++ Backend/TaskManager/src/test/java/com/TaskManagerBackend/TaskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskManagerBackend.TaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
